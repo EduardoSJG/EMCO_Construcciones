f89bee6efccf382e928ccb30c8ee6b8dde146467
Eduardo Juarez <eduardo-j.g@hotmail.com>
2021-04-10 21:55:16 -0500

    AvanceCambios1

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19,7 +19,7 @@
                 $("[name=" + this.hash.slice(1) + "]");
             if (target.length) {
                 $("html, body").animate({
-                        scrollTop: target.offset().top - 72,
+                        scrollTop: target.offset().top - 65,
                     },
                     1000, "easeInOutExpo"
                 );
@@ -52,3 +52,33 @@
         $(window).scroll(navbarCollapse);
     }
 })(jQuery); // End of use strict
+
+function bs_input_file() {
+    $(".input-file").before(
+        function() {
+            if (!$(this).prev().hasClass('input-ghost')) {
+                var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0px;'>");
+                element.attr("name", $(this).attr("name"));
+                element.change(function() {
+                    element.next(element).find('input').val((element.val()).split('\\').pop());
+                });
+                $(this).find("button.btn-choose").click(function() {
+                    element.click();
+                });
+                $(this).find("button.btn-reset").click(function() {
+                    element.val(null);
+                    $(this).parents(".input-file").find('input').val('');
+                });
+                $(this).find('input').css("cursor", "pointer");
+                $(this).find('input').mousedown(function() {
+                    $(this).parents('.input-file').prev().click();
+                    return false;
+                });
+                return element;
+            }
+        }
+    );
+}
+$(function() {
+    bs_input_file();
+});
